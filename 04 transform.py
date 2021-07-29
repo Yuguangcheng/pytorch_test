7ce965008e6aab06dba15d6c242c5e47574c942e
Yuguangcheng <1543053723@qq.com>
2021-07-29 15:35:45 +0800

    tansform commit02

--- 04 transform.py
+++ 04 transform.py
@@ -1,7 +1,10 @@
 from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 img_path = 'hymenoptera_data\\val\\ants\\8124241_36b290d372.jpg'#相对路径
 img = Image.open(img_path)
 tensor_writer = transforms.ToTensor()#创建具体的工具
 img_tensor = tensor_writer(img)
-print(img_tensor)
+writer = SummaryWriter("tsf_logs")
+writer.add_image('tensor_img',img_tensor)
+writer.close()
